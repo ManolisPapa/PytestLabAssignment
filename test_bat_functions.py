f673ccf1484370b095a5e6ffe4d646809722be11
ManolisPapa <manolispapag14@gmail.com>
2025-04-04 15:27:55 +0300

    Added bat_vehicles_dict fixture

--- test_bat_functions.py
+++ test_bat_functions.py
@@ -18,3 +18,13 @@ def test_calculate_bat_power():
 def test_signal_strength(distance, expected):
     # Barcelona 4-1 Girona, Goals by Krejci 43', Lewandowski 61',77', and Ferran Torres 86'
     assert signal_strength(distance) == expected
+
+@pytest.fixture
+def bat_vehicles_dict():
+    return { 
+        'Batmobile': {'speed': 200, 'armor': 80}, 
+        'Batcycle': {'speed': 150, 'armor': 50},
+        'Batplane': {'speed': 250, 'armor': 70}, 
+        'Batboat': {'speed': 180, 'armor': 60},
+        'Batwing': {'speed': 300, 'armor': 60} 
+    }
