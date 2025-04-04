522b6b7156e365503a53b566daef06afdc9e32ab
ManolisPapa <manolispapag14@gmail.com>
2025-04-04 15:26:28 +0300

    Passed bat_power and signal_strength tests

--- test_bat_functions.py
+++ test_bat_functions.py
@@ -1,6 +1,7 @@
 import pytest
 from bat_functions import calculate_bat_power, signal_strength
 
+@pytest.mark.bat_power
 def test_calculate_bat_power():
     # Atletico Madrid 0-1 Barcelona, Goal by Ferran Torres 27'
     assert calculate_bat_power(10) == 420
@@ -13,6 +14,7 @@ def test_calculate_bat_power():
     (12, 0),
     (3.2, 68)
 ])
+@pytest.mark.signal_strength
 def test_signal_strength(distance, expected):
     # Barcelona 4-1 Girona, Goals by Krejci 43', Lewandowski 61',77', and Ferran Torres 86'
     assert signal_strength(distance) == expected
